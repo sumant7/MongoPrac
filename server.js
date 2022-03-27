a74cd7e3440205eab6fb95c704d1abeb36a3de2d
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-27 17:00:28 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const app = express();
+
+const db = config.get('mongoURL');
+
+mongoose.connect(db, 
+    { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true //since node version chosen 2.12 or later
+    })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
