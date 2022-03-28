08c6c435faa66ae37d61e93e810c896354391b0b
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-28 16:04:43 +0530

    schema done

--- models/animal.js
+++ models/animal.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+
+const AnimalSchema = new Schema({
+    name: {
+        type: String, required: true
+    },
+    isEndangered:{
+        type: Boolean, default: false
+    },
+    entrydate: {
+        type: Date, default: Date.now()
+    }
+})
+
+let item= mongoose.model('animal', AnimalSchema)
+module.exports = item
