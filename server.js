598f76fa7cca482b87da71beb25fbfc03d8c011e
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-29 15:50:55 +0530

    CRUD using node

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const app = express();
+const Animal = require('./models/animal');
+//Animal here== animal collection in mongo
 
 const db = config.get('mongoURL');
 
@@ -12,3 +14,32 @@ mongoose.connect(db,
     })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+//create
+const newAnimal = new Animal({
+  name: "Tiger",
+  isEndangered: true
+})
+
+
+//saving
+newAnimal.save()
+.then(item=> console.log('Added'))
+.catch(err=> console.log(err))
+
+
+//reading or finding
+Animal.find().sort({date: -1}).then(items=> console.log(items))
+
+
+//update
+Animal.findByIdAndUpdate({_id: '6242db838b5ae69a48c1ec81'},{
+  isEndangered: false
+}).then(item=> console.log("Updated"))
+
+
+//delete
+Animal.findOneAndDelete({ _id: '5d14cd34e8dbe65a6417915b' },{ isEndangered: false }
+  )
+  .then(console.log('Item deleted'));
