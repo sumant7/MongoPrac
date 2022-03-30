db1fb00ca4dd97fa999cd223cc5a029c423ad33b
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-30 15:08:28 +0530

    get request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const app = express();
 const Animal = require('./models/animal');
+app.use(express.json());
 //Animal here== animal collection in mongo
 
 const db = config.get('mongoURL');
@@ -30,7 +31,7 @@ newAnimal.save()
 
 
 //reading or finding
-Animal.find().sort({date: -1}).then(items=> console.log(items))
+Animal.find().sort({date: -1}).then(items=> console.log('Found'))
 
 
 //update
@@ -43,3 +44,13 @@ Animal.findByIdAndUpdate({_id: '6242db838b5ae69a48c1ec81'},{
 Animal.findOneAndDelete({ _id: '5d14cd34e8dbe65a6417915b' },{ isEndangered: false }
   )
   .then(console.log('Item deleted'));
+
+  
+const port = 5000;
+
+app.get('/', (req, res) => {
+  Animal.find().sort({ date: -1 })
+    .then(items => console.log(res.json(items)));
+});
+
+app.listen(port, () => console.log(`Server started on port: http://localhost:${port}`));
